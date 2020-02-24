d9870c8d29b61de664711b2e36845f75b1ba12fd
Omar Sahtout <omar.sahtout@sap.com>
2020-02-24 17:00:51 -0500

    add buttons to toggle between campuses: #14
    i# fix stuff

--- components/map/map.component.jsx
+++ components/map/map.component.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import MapView from "react-native-maps";
 import { StyleSheet, Dimensions } from "react-native";
 
-const Map = () => {
-  return <MapView style={styles.mapStyle} />;
+const Map = props => {
+  return <MapView style={styles.mapStyle} region={props.region} />;
 };
 
 const styles = StyleSheet.create({
--- screens/map-screen/map-screen.component.jsx
+++ screens/map-screen/map-screen.component.jsx
@@ -2,12 +2,34 @@ import React from "react";
 import { StyleSheet, View } from "react-native";
 
 import Map from "../../components/map/map.component";
+import CampusToggleButtons from "../../components/CampusToggleButtons/CampusToggleButtons.component";
 
 class MapScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      region: {
+        latitude: 45.495869,
+        longitude: -73.578107,
+        latitudeDelta: 0.0522,
+        longitudeDelta: 0.0421
+      }
+    };
+  }
+
+  /**
+   * this will change the value of the coordinates when a toggle button
+   * is pressed or called by any child
+   */
+  onRegionChange = newRegion => {
+    this.setState({ region: newRegion });
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Map />
+        <Map region={this.state.region} />
+        <CampusToggleButtons regionChange={this.onRegionChange} />
       </View>
     );
   }
