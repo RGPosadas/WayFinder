a2867766f7a8b75c6f40c21c16970e1ee00f0c38
JimLau49 <jimmy71471@gmail.com>
2020-02-28 13:01:53 -0500

    Implemented snapshot tests #36 BuildingHighlights and CampusToggle

--- context/region.context.jsx
+++ context/region.context.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const RegionContext = React.createContext(null);
+const RegionContext = React.createContext({});
 
 export const RegionProvider = RegionContext.Provider;
 export const RegionConsumer = RegionContext.Consumer;
