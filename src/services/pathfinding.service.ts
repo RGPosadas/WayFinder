b91682eae36e91c9f573ba0b080fabe046061d16
dabris <dabris16.x@gmail.com>
2020-03-24 11:23:27 -0400

    add documentation & refactor code #45
    add documentation & refactor code #45

--- src/services/pathfinding.service.ts
+++ src/services/pathfinding.service.ts
@@ -4,36 +4,10 @@ import { getDistanceFromLine, getDistance } from "geolib";
 import PriorityQueue from "ts-priority-queue";
 
 /**
- * TASK-14
- * A* implementation for indoor pathfinding
- *
- * TODO:
- *
- * 1. Use traverseNodes() to get a list of lines which correspond to connections between nodes.
- * @see Line
- *
- * 2. Go through each line and check distance between the line and the start location. The
- * line closest to the start location is made up of the travel nodes which are connect to the start location.
- * @see getDistanceFromLine() from geolib.
- *
- * 3. Go through each line and check distance between the line and the end location. The
- * line closest to the end location is made up of the travel nodes which are connect to the start location.
- * @see getDistanceFromLine() from geolib.
- *
- * 4. Make a copy of the floor's travel node list and update it with the new relation found in 2. and 3.
- *
- * 5. Now that the tree is set properly. Do A* (pref. someone who did COMP472 would do this):
- *
- * 1. Initialize the open list with the initial node so (top node)
- * 2. Initialize the closed list to empty
- * 3. Repeat
- *  a) If the open list is empty, then exit with failure.
- *  b) Else, take the first node s from the open list.
- *  c) If s is a goal state, exit with success. Extract the solution path from s to so
- *  d) Else, insert s in the closed list (s has been visited /expanded)
- *  e) Insert the successors of s in the open list in a certain order if
- *  they are not already in the closed and/or open lists (to avoid
- *  cycles)
+ * Find the shortest path between two locations on a given floor
+ * @param floorId
+ * @param start
+ * @param end
  */
 export const findPathOnFloor = (
   floorId: number,
@@ -65,6 +39,28 @@ export const findPathOnFloor = (
   nodes[endEdge[0].id].children.push(goal.id);
   nodes[endEdge[1].id].children.push(goal.id);
 
+  let searchPaths = search(nodes, initial, goal);
+  let shortestPath: Line[] = [];
+  let id: number = goal.id;
+  while (id != initial.id) {
+    let parentId: number = searchPaths[id];
+    shortestPath.push([nodes[id], nodes[parentId]]);
+    id = parentId;
+  }
+  return shortestPath;
+};
+
+/**
+ * A* implementation for indoor pathfinding
+ * @param nodes
+ * @param initial
+ * @param goal
+ */
+const search = (
+  nodes: TravelNode[],
+  initial: TravelNode,
+  goal: TravelNode
+): {} => {
   let closed = {};
   let open: PriorityQueue<PQItem> = new PriorityQueue<PQItem>({
     comparator: (a, b) => {
@@ -85,15 +81,7 @@ export const findPathOnFloor = (
       open.queue({ id: child, parent: current.id, g: g });
     });
   }
-
-  let path: Line[] = [];
-  let id: number = goal.id;
-  while (id != initial.id) {
-    let parentId: number = closed[id];
-    path.push([nodes[id], nodes[parentId]]);
-    id = parentId;
-  }
-  return path;
+  return closed;
 };
 
 const findEdge = (edges: Line[], point: Location): Line => {
